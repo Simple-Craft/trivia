fd12cc96ff730fffa952499b1af2db1b91652354
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-07-25 22:01:26 +1000

    Authentication middleware

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,6 @@ import apiRoutes from "./routes/api"
 import SequelizeStore from "connect-session-sequelize"
 import sequelize from "./sequelize"
 
-const app = express()
-
 // Configure session
 let SequelizeSessionStore = SequelizeStore(session.Store)
 let seq_store = new SequelizeSessionStore({ db: sequelize })
@@ -22,9 +20,42 @@ let sess = {
     resave: false,
     saveUninitialized: false
 }
+/* Enable this again when HTTPS is setup
 if (process.env.ENVIRONMENT === 'production') {
     sess.cookie.secure = true
 }
+*/
+
+interface ResponseError extends Error {
+    status?: number;
+}
+
+// Middleware that can be used for authentication in other routes
+function requireLogin(req: express.Request, res: express.Response, next: express.NextFunction) {
+    if (!req.session.user) {
+        let err = new Error('Unauthorized') as ResponseError
+        err.status = 401
+        throw err
+    }
+    next()
+}
+
+function requireAdmin(req: express.Request, res: express.Response, next: express.NextFunction) {
+    if (!req.session.user) {
+        let err = new Error() as ResponseError
+        err.status = 404
+        throw err
+    }
+
+    if (!req.session.user.admin) {
+        let err = new Error() as ResponseError
+        err.status = 404
+        throw err
+    }
+    next()
+}
+
+const app = express()
 app.use(session(sess))
 
 // Routing
@@ -32,4 +63,4 @@ app.use('/user', userRoutes)
 app.use('/admin', adminRoutes)
 app.use('/api', apiRoutes)
 
-export { app }
+export { app, requireLogin, requireAdmin }
