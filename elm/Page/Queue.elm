93c8b006e808b2f7d64c74e4236e75dc0d824a91
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-07-29 00:11:11 +1000

    Admin queue editing

--- elm/Page/Queue.elm
+++ elm/Page/Queue.elm
@@ -1,16 +1,20 @@
 module Page.Queue exposing (Model, Msg(..), init, update, view)
 
-import Html exposing (table, td, text, th, thead, tr)
+import Array exposing (Array)
+import Html exposing (input, table, td, text, th, thead, tr)
+import Html.Attributes exposing (type_, value)
+import Html.Events exposing (stopPropagationOn, targetValue)
 import Http
 import HttpBuilder
 import Json.Decode as Decode exposing (int, string)
 import Json.Decode.Pipeline exposing (required)
+import Json.Encode as Encode
 
 
 type alias Model =
     { page : Int
     , maxpage : Int
-    , questions : List Question
+    , questions : Array Question
     }
 
 
@@ -21,14 +25,21 @@ type alias Question =
     , difficulty : String
     , submitter : String
     , correctAnswer : String
-    , wrongAnswers : List String
+    , wrongAnswers : Array String
     , submissionTime : String
     }
 
 
+type QuestionProperty
+    = QuestionText
+    | CorrectAnswer
+    | WrongAnswer Int
+    | Difficulty
+
+
 type alias QuestionPage =
     { maxpage : Int
-    , questions : List Question
+    , questions : Array Question
     }
 
 
@@ -41,33 +52,45 @@ decodeQuestion =
         |> required "difficulty" string
         |> required "submitter" string
         |> required "correctAnswer" string
-        |> required "wrongAnswers" (Decode.list string)
+        |> required "wrongAnswers" (Decode.array string)
         |> required "submissionTime" string
+
+
+encodeQuestion : Question -> Encode.Value
+encodeQuestion question =
+    Encode.object
+        [ ( "question", Encode.string question.question )
+        , ( "difficulty", Encode.string question.difficulty )
+        , ( "correctAnswer", Encode.string question.correctAnswer )
+        , ( "wrongAnswers", Encode.array Encode.string question.wrongAnswers )
+        ]
 
 
 decodeQuestionPage : Decode.Decoder QuestionPage
 decodeQuestionPage =
     Decode.succeed QuestionPage
         |> required "page_count" int
-        |> required "questions" (Decode.list decodeQuestion)
+        |> required "questions" (Decode.array decodeQuestion)
 
 
 type Msg
     = NoOp
     | LoadedQuestionData (Result Http.Error QuestionPage)
+    | UpdateQuestion Int QuestionProperty String
+    | UpdateQuestionResult (Result Http.Error ())
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { page = 0
       , maxpage = 0
-      , questions = []
+      , questions = Array.empty
       }
     , getQuestions 0
     )
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         LoadedQuestionData result ->
@@ -78,8 +101,44 @@ update msg model =
                 Ok page ->
                     ( { model | maxpage = page.maxpage, questions = page.questions }, Cmd.none )
 
+        UpdateQuestion idx property value ->
+            let
+                question =
+                    Array.get idx model.questions
+            in
+            case question of
+                Just q ->
+                    let
+                        ( updatedQuestion, updatedModel ) =
+                            updateQuestion model q idx property value
+                    in
+                    ( updatedModel, postUpdateQuestion q.id updatedQuestion )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
         _ ->
             ( model, Cmd.none )
+
+
+updateQuestion : Model -> Question -> Int -> QuestionProperty -> String -> ( Question, Model )
+updateQuestion model question idx property value =
+    let
+        updatedQuestion =
+            case property of
+                QuestionText ->
+                    { question | question = value }
+
+                CorrectAnswer ->
+                    { question | correctAnswer = value }
+
+                WrongAnswer i ->
+                    { question | wrongAnswers = Array.set i value question.wrongAnswers }
+
+                Difficulty ->
+                    { question | difficulty = value }
+    in
+    ( updatedQuestion, { model | questions = Array.set idx updatedQuestion model.questions } )
 
 
 getQuestions : Int -> Cmd Msg
@@ -91,14 +150,22 @@ getQuestions page =
         |> HttpBuilder.request
 
 
-view : Model -> List (Html.Html msg)
+postUpdateQuestion : Int -> Question -> Cmd Msg
+postUpdateQuestion id question =
+    ("/admin/edit/" ++ String.fromInt id)
+        |> HttpBuilder.post
+        |> HttpBuilder.withTimeout 1000
+        |> HttpBuilder.withJsonBody (encodeQuestion question)
+        |> HttpBuilder.withExpect (Http.expectWhatever UpdateQuestionResult)
+        |> HttpBuilder.request
+
+
+view : Model -> Html.Html Msg
 view model =
-    [ Html.text "Question Queue"
-    , Html.table [] (viewHeader :: viewRows model)
-    ]
+    Html.table [] (viewHeader :: viewRows model)
 
 
-viewHeader : Html.Html msg
+viewHeader : Html.Html Msg
 viewHeader =
     thead []
         [ th [] [ text "ID" ]
@@ -112,20 +179,49 @@ viewHeader =
         ]
 
 
-viewRows : Model -> List (Html.Html msg)
+viewRows : Model -> List (Html.Html Msg)
 viewRows { questions } =
-    List.map viewRow questions
+    Array.toList (Array.indexedMap viewRow questions)
 
 
-viewRow : Question -> Html.Html msg
-viewRow question =
+viewRow : Int -> Question -> Html.Html Msg
+viewRow idx question =
     tr []
         [ td [] [ text (String.fromInt question.id) ]
-        , td [] [ text question.question ]
+        , td [] [ viewInput idx QuestionText question.question ]
         , td [] [ text question.category ]
         , td [] [ text question.difficulty ]
-        , td [] [ text question.correctAnswer ]
-        , td [] (List.map text question.wrongAnswers)
+        , td [] [ viewInput idx CorrectAnswer question.correctAnswer ]
+        , td [] (viewWrongAnswers idx question)
         , td [] [ text question.submitter ]
         , td [] []
         ]
+
+
+viewWrongAnswers : Int -> Question -> List (Html.Html Msg)
+viewWrongAnswers idx question =
+    Array.indexedMap (viewWrongAnswer idx) question.wrongAnswers
+        |> Array.toList
+        |> List.concat
+
+
+viewWrongAnswer : Int -> Int -> String -> List (Html.Html Msg)
+viewWrongAnswer idx wrongIndex text =
+    [ viewInput idx (WrongAnswer wrongIndex) text
+    , Html.br [] []
+    ]
+
+
+viewInput : Int -> QuestionProperty -> String -> Html.Html Msg
+viewInput idx property v =
+    input [ type_ "text", value v, onChange (UpdateQuestion idx property) ] []
+
+
+onChange : (String -> msg) -> Html.Attribute msg
+onChange tagger =
+    stopPropagationOn "change" (Decode.map alwaysStop (Decode.map tagger targetValue))
+
+
+alwaysStop : a -> ( a, Bool )
+alwaysStop x =
+    ( x, True )
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,12 +3,13 @@ import { requireAdmin } from "../middleware"
 import path from "path"
 
 import Question from "../models/Question.model"
-import { ApprovalState, Difficulty } from "../models/Question.model"
+import { ApprovalState, Difficulty, stringToDifficulty } from "../models/Question.model"
 import User from "../models/User.model"
 import Category from "../models/Category.model"
 
 const router = express.Router()
 router.use(requireAdmin)
+router.use(express.json())
 
 function mapQuestion(question: Question) {
     return {
@@ -42,6 +43,19 @@ router.get('/queue/:page', async (req, res) => {
         page_count: page_count,
         questions: questions.map(mapQuestion)
     })
+})
+
+router.post('/edit/:id', async (req, res) => {
+    console.log(req.body)
+    let question = await Question.findOne({ where: { id: req.params.id } })
+
+    question.question = req.body.question || question.question
+    question.correctAnswer = req.body.correctAnswer || question.correctAnswer
+    question.difficulty = stringToDifficulty(req.body.difficulty) || question.difficulty
+    question.wrongAnswers = req.body.wrongAnswers || question.wrongAnswers
+    question.save()
+
+    res.sendStatus(200)
 })
 
 // Internal routing handled by Elm
